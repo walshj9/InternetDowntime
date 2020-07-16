86e79a8d9f6f520c209bbe9a4fdaaa766c3b6f87
walshj9 <walshj9@students.rowan.edu>
2020-07-15 23:09:56 -0400

    Added timeout for ping connection test

--- conntest.sh
+++ conntest.sh
@@ -2,12 +2,16 @@
 
 now=$(date)
 
-
-ping_result=$(ping -c1 www.google.com | tail -1 | awk '{print $4}' | cut -d '/' -f 2)
-clean_result=${ping_result%.*}
-
-if [[ "$clean_result" -gt 0 ]]
-then
-	echo "There is an internet connection"
-fi
-
+connected= true
+#while $connected:
+#do
+	 
+	ping_result=$(timeout 3  ping -c1 www.google.com | tail -1 | awk '{print $4}' | cut -d '/' -f 2)
+	clean_result=${ping_result%.*}
+	
+	if [[ "$clean_result" -gt 0 ]]; then
+		echo "Ping: $clean_result"
+	else
+		echo "No Connection"
+	fi
+#done
